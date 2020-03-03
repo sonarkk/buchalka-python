78b439431fa7c3edf1f03e221ddf014f482fe67f
sonarkk <56376342+sonarkk@users.noreply.github.com>
2020-03-02 21:14:20 -0800

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,11 @@
+print("Today is a good day to learn Python")
+print('Python is fun')
+print("Python's strings are easy to use")
+print('We can even include "quotes" in strings')
+print("hello" + " world")
+greeting = "Hello"
+name = input("Please enter your name ")
+print (greeting + name)
+
+#If we want a space, we can add that too.
+print(greeting + ' ' + name)
