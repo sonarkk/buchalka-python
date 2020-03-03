67280dc53a5442681f4cd84e91ce7f7874bd930d
sonarkk <56376342+sonarkk@users.noreply.github.com>
2020-03-02 23:58:11 -0800

    Add files via upload

--- strings.py
+++ strings.py
@@ -4,8 +4,19 @@ print("Python's strings are easy to use")
 print('We can even include "quotes" in strings')
 print("hello" + " world")
 greeting = "Hello"
-name = input("Please enter your name ")
-print (greeting + name)
+name = "Mia"
+print(greeting + name)
 
 #If we want a space, we can add that too.
 print(greeting + ' ' + name)
+
+
+age = 24
+print(age)
+
+print(type(greeting))
+print(type(age))
+
+age_in_words = "2 years"
+print(name + " is" + age + " years old")
+print(type(age))
