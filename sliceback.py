a45ac68031093dd435ec2141adba2525163fc02e
sonarkk <56376342+sonarkk@users.noreply.github.com>
2020-03-03 22:57:59 -0800

    Add files via upload

---
+++ sliceback.py
@@ -0,0 +1,19 @@
+letters = ""
+
+backwards = letters[::-1]
+print(backwards)
+
+# qpo
+qpo = letters[-10:-13:-1]
+print(qpo)
+# edcba
+edcba = letters[-22::-1]
+print(edcba)
+# last 8 letters in reverse
+last = letters[:-9:-1]
+print(last)
+
+print(letters[-4:])
+print(letters[-1:])
+print(letters[:1])
+print(letters[0])
