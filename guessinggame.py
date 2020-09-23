83846609509a4ecfeedbd7232fde00416f28b5ea
sonarkk <56376342+sonarkk@users.noreply.github.com>
2020-09-22 17:15:11 -0700

    Add files via upload

--- guessinggame.py
+++ guessinggame.py
@@ -1,11 +1,18 @@
-answer = 5
+import random
 
-print("Please guess a number between 1 and 10: ")
+highest = 1000
+answer = random.randint(1, highest)
+print(answer)   #TODO: Remove after testing
+
+print("Please guess a number between 1 and {}: ".format(highest))
 guess = int(input())
 
 if guess == answer:
     print("You got it first time")
-else:
+while guess != answer:
+    if guess == 0:
+        print("Game over")
+        break
     if guess < answer:
         print("Please guess higher")
     else:   #guess must be greater than answer
@@ -16,32 +23,3 @@ else:
     else:
         print("Sorry, you have not guessed correctly")
 
-# if guess != answer:
-#     if guess < answer:
-#         print("Please guess higher")
-#     else:   #guess must be greater than answer
-#         print("Please guess lower")
-#     guess = int(input())
-#     if guess == answer:
-#         print("Well done, you guessed it")
-#     else:
-#         print("Sorry, you have not guessed correctly")
-# else:
-#     print("You got it first time")
-
-# if guess < answer:
-#     print("Please guess higher")
-#     guess = int(input())
-#     if guess == answer:
-#         print("Well done, you guessed it")
-#     else:
-#         print("Sorry, you have not guessed correctly")
-# elif guess > answer:
-#     print("Please guess lower")
-#     guess = int(input())
-#     if guess == answer:
-#         print("Well done, you guessed it")
-#     else:
-#         print("Sorry, you have not guessed correctly")
-# else:
-#     print("You got it first time")
